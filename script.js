8c6e223da396fbd86681156b56b33d1acf01c0ee
Bugdoubt <kieronmillar@hotmail.com>
2025-04-18 11:18:41 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -5,40 +5,62 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
-const cities = {
-  "New York": [40.7128, -74.0060],
-  "London": [51.5074, -0.1278],
-  "Paris": [48.8566, 2.3522],
-  "Tokyo": [35.6762, 139.6503]
+const cityImages = {
+  "New York": {
+    coords: [40.758, -73.9855],
+    images: [
+      "https://upload.wikimedia.org/wikipedia/commons/4/4f/Times_Square_New_York_City_%28HDR%29.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/5/57/NYC_Times_Square_wide_angle.jpg"
+    ]
+  },
+  "London": {
+    coords: [51.5033, -0.1195],
+    images: [
+      "https://upload.wikimedia.org/wikipedia/commons/e/e1/London_Eye_Twilight_April_2006.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/f/fd/Big_Ben_2012-06-25.jpg"
+    ]
+  },
+  "Paris": {
+    coords: [48.8584, 2.2945],
+    images: [
+      "https://upload.wikimedia.org/wikipedia/commons/a/af/Paris_vue_d%27ensemble_tour_Eiffel.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/6/6e/Cathedrale_Notre-Dame_de_Paris.jpg"
+    ]
+  }
 };
 
-for (const [name, coords] of Object.entries(cities)) {
-  L.marker(coords).addTo(map)
+let currentCity = null;
+let currentImageIndex = 0;
+
+for (const [name, data] of Object.entries(cityImages)) {
+  L.marker(data.coords).addTo(map)
     .bindPopup(name)
     .on('click', () => {
+      currentCity = name;
       currentImageIndex = 0;
       updateImage();
     });
 }
 
-const images = [
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/June_odd-eyed-cat.jpg/640px-June_odd-eyed-cat.jpg",
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/640px-Cat03.jpg",
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/VAN_CAT.png/640px-VAN_CAT.png"
-];
-
-let currentImageIndex = 0;
-
 function updateImage() {
-  document.getElementById("photo").src = images[currentImageIndex];
+  if (currentCity) {
+    const imageUrl = cityImages[currentCity].images[currentImageIndex];
+    document.getElementById("photo").src = imageUrl;
+  }
 }
 
 function nextImage() {
-  currentImageIndex = (currentImageIndex + 1) % images.length;
-  updateImage();
+  if (currentCity) {
+    const imgs = cityImages[currentCity].images;
+    currentImageIndex = (currentImageIndex + 1) % imgs.length;
+    updateImage();
+  }
 }
 
 function prevImage() {
-  currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
-  updateImage();
+  if (currentCity) {
+    const imgs = cityImages[currentCity].images;
+    currentImageIndex = (currentImageIndex - 1 + imgs.length) % imgs.length;
+    updateImage();
+  }
 }
