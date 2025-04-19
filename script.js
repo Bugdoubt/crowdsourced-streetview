08f25c5d89f15fb384122042590b13d1f7e033c9
Bugdoubt <kieronmillar@hotmail.com>
2025-04-19 11:44:34 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 
-const notredameImages = [
-  "https://images.unsplash.com/photo-1564501049412-61c2a3083793?auto=format&fit=crop&w=900&q=80",
-  "https://images.unsplash.com/photo-1589891455809-5b9d27bbf70d?auto=format&fit=crop&w=900&q=80",
-  "https://images.unsplash.com/photo-1566764629489-1b6b68d8a0e7?auto=format&fit=crop&w=900&q=80",
-  "https://images.unsplash.com/photo-1580235544729-91a989ed915e?auto=format&fit=crop&w=900&q=80"
-];
+const notredameImages = ['https://upload.wikimedia.org/wikipedia/commons/b/b6/Paris_Notre-Dame_Vaults_01.JPG', 'https://upload.wikimedia.org/wikipedia/commons/e/e6/Notre_Dame_de_Paris_-_Facade_2017.jpg', 'https://upload.wikimedia.org/wikipedia/commons/4/4b/Notre_Dame_de_Paris_-_Nave.jpg', 'https://upload.wikimedia.org/wikipedia/commons/6/6e/Cathedrale_Notre-Dame_de_Paris.jpg'];
 
 let currentIndex = 0;
 
