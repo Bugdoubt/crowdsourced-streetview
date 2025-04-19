ba85070dc464aab63384805d136bb420c313f26c
Bugdoubt <kieronmillar@hotmail.com>
2025-04-19 11:12:19 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -6,16 +6,16 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 const londonImages = [
-  "https://live.staticflickr.com/65535/53661293729_3a905f5829_z.jpg",
-  "https://live.staticflickr.com/65535/53661159128_9e6a0aa307_z.jpg",
-  "https://live.staticflickr.com/65535/53660806696_67ae820d88_z.jpg",
-  "https://live.staticflickr.com/65535/53660811576_1cce6e44fa_z.jpg"
+  "https://images.unsplash.com/photo-1570129477492-45c003edd2be?auto=format&fit=crop&w=800&q=80",
+  "https://images.unsplash.com/photo-1506459225024-1428097a7e18?auto=format&fit=crop&w=800&q=80",
+  "https://images.unsplash.com/photo-1533681018184-e070d38e8de3?auto=format&fit=crop&w=800&q=80",
+  "https://images.unsplash.com/photo-1555066931-4365d14bab8c?auto=format&fit=crop&w=800&q=80"
 ];
 
 let currentIndex = 0;
 
 const marker = L.marker([51.5074, -0.1278]).addTo(map)
-  .bindPopup("London")
+  .bindPopup("London (Unsplash)")
   .on("click", () => {
     currentIndex = 0;
     updateImage();
